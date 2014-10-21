00b28cf24aab2cf6dcbe33b0b52791a0f58031c9
Dustin Raimondi <raimondi1337@gmail.com>
2014-10-20 23:08:29 -0400

    Added up botton to bottom section Removed inline click events Made pictures clickable Changed toggle logic and click events to work with new design, tabs are no longer clickable in bottom view

--- site/js/main.js
+++ site/js/main.js
@@ -1,18 +1,48 @@
 var atTop = true;
 
 window.onload=function(){
+	//hide the bottom section and the up
 	$('#bottom').hide();
+	$('#upButton').hide();
+
+	//create alide toggles on click
+	$('#upButton').click(function(){
+		toggle();
+	});
+	$('#upButton').children().addClass("clickable");
+	$('#tabs').children().on("click", function(){
+		toggle();
+	});
+	$('#tabs').children().addClass("clickable");
+	$('#pics').children().on("click", function(){
+		toggle();
+	});
+	$('#pics').children().addClass("clickable");
 }
 
 function toggle(){
 		$( "#top" ).slideToggle("fast");
 		$( "#bottom" ).slideToggle("fast");
+		$( "#upButton" ).slideToggle("fast");
 
 		if(atTop){
 			$("html, body").animate({ scrollTop: 0 }, "fast");
+			$("#tabs").children().off("click");
+			$('#tabs').children().removeClass("clickable");
+			$("#pics").children().off("click");
+			$('#pics').children().removeClass("clickable");
 			atTop=false;
 		} else {
 			$("html, body").animate({ scrollTop: $(document).height() }, "fast");
+			$("#tabs").children().on("click", function(){
+				toggle();
+			});
+			$('#tabs').children().addClass("clickable");
+			$('#tabs').children().addClass("clickable");
+			$('#pics').children().on("click", function(){
+				toggle();
+			});
+			$('#pics').children().addClass("clickable");
 			atTop=true;
 		}
 }
